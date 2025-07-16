4c1ef8bda22ee407e35059463a8e736899d55fa0
RamAgrawal01 <bansalram7847@gmail.com>
2025-07-16 13:21:19 +0530

    Mongo db improved

--- utils/db.js
+++ utils/db.js
@@ -2,13 +2,20 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 
-const connectDB = async() => {
-    try {
-        await mongoose.connect(process.env.MONGO_URI);
-        console.log("server will be connected");
-    } catch (error) {
-        console.error("MongoDB connection error:", error);
-    }
-}
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    mongoose.set("strictQuery", false);
+    mongoose.set("bufferCommands", false); // prevents buffering issue
+    console.log("✅ MongoDB connected");
+  } catch (error) {
+    console.error("❌ MongoDB connection error:", error.message);
+    process.exit(1);
+  }
+};
 
 export default connectDB;
